ea5649b6d82d300fda11391ee0cc608ff783f108
Bensha025 <157089346+Bensha025@users.noreply.github.com>
2025-11-25 15:32:11 -0600

    Add files via upload

---
+++ Hands-on 4/input.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define SCALE_FACTOR 2
+
+int globalA;
+int globalB;
+
+int addValues(int first, int second) {
+    int resultLocal;
+    resultLocal = first + second;
+    return resultLocal;
+}
+
+int processValue(int value) {
+    int temporaryVal;
+    temporaryVal = value * SCALE_FACTOR;
+    {
+        int innerResult;
+        innerResult = temporaryVal + 5;
+        printf("%d\n", innerResult);
+    }
+    return temporaryVal;
+}
+
+int main(void) {
+    int resultMain;
+    int auxValue;
+
+    globalA = 3;
+    globalB = 4;
+
+    resultMain = addValues(globalA, globalB);
+    printf("%d\n", resultMain);
+
+    auxValue = processValue(resultMain);
+    printf("%d\n", auxValue);
+
+    {
+        int finalOutput;
+        finalOutput = auxValue + resultMain;
+        printf("%d\n", finalOutput);
+    }
+
+    return 0;
+}
+
